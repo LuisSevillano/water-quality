9e1fe670b78e0d8a7780f17f664e44882c477511
LuisSevillano <luissevillanopires@gmail.com>
2023-11-23 10:13:51 +0100

    Update packages (II)

--- R/00_prepare.R
+++ R/00_prepare.R
@@ -1,7 +1,9 @@
-library(dplyr)
-library(stringr)
-library(rvest)
-library(lubridate)
+library('dplyr')
+library('stringr')
+library('rvest')
+library('lubridate')
+library('readr')
+library('ggplot2')
 
 source('R/01_scrape.R')
 source('R/02_plot.R')
